f583a337dc1871b8afcac523f09e324e70cc58c9
FVS-MBA <feliven@gmail.com>
2025-11-14 21:30:47 -0300

    instalado FontAwesomeModule

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-item',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './item.component.html',
   styleUrl: './item.component.css',
+  standalone: true,
 })
 export class ItemComponent {
   faPen = faPen;
